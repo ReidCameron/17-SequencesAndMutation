44bb7b8c4f4c09e6911bf44cbcd990943cb5b753
ReidCameron <reidcj@rose-hulman.edu>
2018-04-23 19:17:07 -0400

    Done.

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,16 +10,16 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Cameron Reid.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_shortest_string()
-    run_test_index_of_largest_number()
-    run_test_number_of_stutters()
-    run_test_is_palindrome()
+    # run_test_shortest_string()
+    # run_test_index_of_largest_number()
+    # run_test_number_of_stutters()
+    # run_test_is_palindrome()
     run_test_count_same()
 
 
@@ -103,10 +103,15 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    index = 0
+    for k in range(1,len(strings)):
+        if len(strings[k]) < len(strings[index]):
+            index = k
+    return strings[index]
 
 def run_test_index_of_largest_number():
     """ Tests the   index_of_largest_number   function. """
@@ -180,9 +185,15 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    index = 0
+    for k in range(1,n):
+        if numbers[k] > numbers[index]:
+            index = k
+    return index
 
 
 # ----------------------------------------------------------------------
@@ -235,10 +246,15 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    count = 0
+    for k in range(len(s)-1):
+        if s[k] == s[k+1]:
+            count = count + 1
+    return count
 
 def run_test_is_palindrome():
     """ Tests the   is_palindrome   function. """
@@ -308,7 +324,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -317,6 +333,13 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+
+    for k in range(len(s)//2):
+        if s[k] != s[len(s)-(k+1)]:
+            return False
+    return True
+
+
 
 
 # ----------------------------------------------------------------------
@@ -379,6 +402,11 @@ def count_same(sequence1, sequence2):
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    count = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            count = count + 1
+    return count
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
